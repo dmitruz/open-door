c44489c71f84d5ee20063262468d154c0626761f
dmitruz <dmitruz970@gmail.com>
2023-05-30 14:03:03 +0300

    add logo

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Logo from '../../images/logo.png';
 
  const Header = () => {
   return (
     <header>
     <div className='container flex'>
         <div className='logo'>
-            <img src="" alt="logo" />
+           <Logo /> 
         </div>
     </div>
     </header>
