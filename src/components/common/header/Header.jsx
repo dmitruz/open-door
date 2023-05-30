b1b10f2e914714b290dae5148f2a13d3711ea075
dmitruz <dmitruz970@gmail.com>
2023-05-30 15:05:24 +0300

    add styles in header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { nav } from '../../data/data'
+import React, { useState } from 'react';
+import "./header.css";
+import { Link } from 'react-router-dom';
+import { nav } from '../../data/data';
 
  const Header = () => {
+    const [navList, setNavList] = useState(false);
   return (
     <>
     <header>
@@ -11,18 +13,30 @@ import { nav } from '../../data/data'
            <img src="../../images/logo.png" alt="logo" /> 
         </div>
         <div className='nav'>
-            <ul className='flex'>
-                {nav.map((list, index) => (
-                    <li key={index}>
-                        <Link to={list.path}>{list.text}</Link>
-                    </li>
-                ))}
+             <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
             </ul>
+          </div>
+          <div className='button flex'>
+            <h4>
+              <span>2</span> My List
+            </h4>
+            <button className='btn1'>
+              <i className='fa fa-sign-out'></i> Sign In
+            </button>
+          </div>
+
+          <div className='toggle'>
+            <button onClick={() => setNavList(!navList)}>{navList ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}</button>
+          </div>
         </div>
-    </div>
-    </header>
+      </header>
     </>
   )
 }
 
-export default Header;
+export default Header
