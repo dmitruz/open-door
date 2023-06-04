c47b4424663dddbbe95e32ef1ae7be2c32a1bae0
dmitruz <dmitruz970@gmail.com>
2023-06-04 11:35:47 +0300

    add images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import Back from "../common/Back";
+import Heading from "../common/Heading";
+import img from "../images/about.jpg";
+import "./about.css"
 const About = () => {
     return (
         <h1>About</h1>
