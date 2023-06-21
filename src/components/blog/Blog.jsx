afb578025d0e030024cabde6cc85adb4e488fc52
dmitruz <dmitruz970@gmail.com>
2023-06-21 12:37:16 +0300

    blog connect styles

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,6 +1,19 @@
+
+import React from "react";
+import Back from "../common/Back";
+import RecentCard from "../home/recent/RecentCard";
+import img from "../images/about.jpg";
+
 const Blog = () => {
     return (
-        <h1>About</h1>
+        <>
+         <section className='blog-out mb'>
+        <Back name='Blog' title='Blog Grid - Our Blogs' cover={img} />
+        <div className='container recent'>
+          <RecentCard />
+        </div>
+      </section>
+        </>
     )
 }
 
